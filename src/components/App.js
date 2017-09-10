742c6fbfbdb1f1fcceaf14f358b97aed2a39584f
Robin Dickson <robindickson@gmail.com>
2017-09-10 16:04:47 +0200

    feat: Add home and category routes

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+import { getCategories } from '../utils/api'
+
+class App extends Component {
+  state = {
+    categories: [],
+  }
+  componentDidMount() {
+    getCategories().then((categories) => {
+      this.setState({ categories });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Readable</h2>
+        </div>
+        <div>
+          <ul>
+            {this.state.categories.map((category) => (
+              <li key={category.name}>{category.name}</li>
+            ))}
+          </ul>
+        </div>
+        <Route exact path="/" render={() => (
+          <div>
+            Hello World!
+          </div>
+        )}/>
+        <Route path="/:category" render={() => (
+          <div>
+            A category..!
+          </div>
+        )}/>
+      </div>
+    );
+  }
+}
+
+export default App;
