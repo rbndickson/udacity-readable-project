f563af4e865821b5d1121ad16e37fb5224ef1d7f
Robin Dickson <robindickson@gmail.com>
2017-09-12 15:06:14 +0200

    fix: Update category filter in store w/ using URL

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,15 @@ import PostList from './PostList';
 import { changeCategoryFilter } from '../actions';
 
 class Main extends Component {
-  componentDidUpdate() {
+  updateCategoryFilter() {
     const category = this.props.match.params.category || 'all'
 
-    this.props.dispatch(
-      changeCategoryFilter(category)
-    )
+    this.props.dispatch(changeCategoryFilter(category))
   }
+
+  // For ariving via the URL & Using the navigation
+  componentDidMount() { this.updateCategoryFilter() }
+  componentDidUpdate() { this.updateCategoryFilter() }
 
   render() {
     return (
