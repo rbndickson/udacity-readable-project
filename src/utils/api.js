b4d68bf4ced5c73ad6044e4d3181b8f125e5a1fa
Robin Dickson <robindickson@gmail.com>
2018-05-26 23:05:23 +0700

    style: Update utils JS style with Prettier

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,127 +1,125 @@
-const api = 'http://localhost:3001'
+const api = "http://localhost:3001";
 
-let token = localStorage.token
+let token = localStorage.token;
 
 if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
+  token = localStorage.token = Math.random()
+    .toString(36)
+    .substr(-8);
 
 const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
-}
+  Accept: "application/json",
+  Authorization: token
+};
 
 export const getCategories = () =>
   fetch(`${api}/categories`, { headers })
     .then(res => res.json())
-    .then(data => data.categories)
+    .then(data => data.categories);
 
-export const getPost = (postId) =>
-  fetch(`${api}/posts/${postId}`, { headers })
-    .then(res => res.json())
+export const getPost = postId =>
+  fetch(`${api}/posts/${postId}`, { headers }).then(res => res.json());
 
 export const getPosts = () =>
-  fetch(`${api}/posts`, { headers })
-    .then(res => res.json())
+  fetch(`${api}/posts`, { headers }).then(res => res.json());
 
-
-export const createPost = (postParams) =>
+export const createPost = postParams =>
   fetch(`${api}/posts`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(postParams)
-  }).then(res => res.json())
+  }).then(res => res.json());
 
 export const updatePost = (postId, postParams) =>
   fetch(`${api}/posts/${postId}`, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(postParams)
-  }).then(res => res.json())
+  }).then(res => res.json());
 
-export const deletePost = (postId) =>
+export const deletePost = postId =>
   fetch(`${api}/posts/${postId}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
-    },
-  })
+      "Content-Type": "application/json"
+    }
+  });
 
-export const getPostComments = (postId) =>
-  fetch(`${api}/posts/${postId}/comments`, { headers })
-    .then(res => res.json())
+export const getPostComments = postId =>
+  fetch(`${api}/posts/${postId}/comments`, { headers }).then(res => res.json());
 
-export const upVotePost = (postId) =>
+export const upVotePost = postId =>
   fetch(`${api}/posts/${postId}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({option: 'upVote'})
-  }).then(res => res.json())
+    body: JSON.stringify({ option: "upVote" })
+  }).then(res => res.json());
 
-export const downVotePost = (postId) =>
+export const downVotePost = postId =>
   fetch(`${api}/posts/${postId}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({option: 'downVote'})
-  }).then(res => res.json())
+    body: JSON.stringify({ option: "downVote" })
+  }).then(res => res.json());
 
-export const createComment = (commentParams) =>
+export const createComment = commentParams =>
   fetch(`${api}/comments`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(commentParams)
-  }).then(res => res.json())
+  }).then(res => res.json());
 
 export const updateComment = (commentId, commentParams) =>
   fetch(`${api}/comments/${commentId}`, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(commentParams)
-  }).then(res => res.json())
+  }).then(res => res.json());
 
-export const deleteComment = (commentId) =>
+export const deleteComment = commentId =>
   fetch(`${api}/comments/${commentId}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
-    },
-  })
+      "Content-Type": "application/json"
+    }
+  });
 
-export const upVoteComment = (commentId) =>
+export const upVoteComment = commentId =>
   fetch(`${api}/comments/${commentId}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({option: 'upVote'})
-  }).then(res => res.json())
+    body: JSON.stringify({ option: "upVote" })
+  }).then(res => res.json());
 
-export const downVoteComment = (commentId) =>
+export const downVoteComment = commentId =>
   fetch(`${api}/comments/${commentId}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({option: 'downVote'})
-  }).then(res => res.json())
+    body: JSON.stringify({ option: "downVote" })
+  }).then(res => res.json());
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,6 +2,6 @@ export function capitalize(str) {
   if (str.length) {
     return str[0].toUpperCase() + str.slice(1).toLowerCase();
   } else {
-    return '';
+    return "";
   }
 }
