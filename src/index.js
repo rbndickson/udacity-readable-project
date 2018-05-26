c02a0fe0fefbb2376e0ace38d417920489a2a6cb
Robin Dickson <robindickson@gmail.com>
2018-05-26 23:12:01 +0700

    style: Update index.js styles with Prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import reducer from './reducers'
-import { Provider } from 'react-redux';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import reducer from "./reducers";
+import { Provider } from "react-redux";
 
 const store = createStore(
   reducer,
@@ -18,5 +18,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
