f203f9c3eb594704a0172479847906c7b25a4a18
Robin Dickson <robindickson@gmail.com>
2018-05-28 02:24:19 +0700

    refactor: Use Formik to edit on edit post form

--- src/components/EditPostForm.js
+++ src/components/EditPostForm.js
@@ -1,75 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
+import { withFormik, Form, Field } from "formik";
 import "./EditPostForm.css";
 import { updatePost } from "../utils/api";
 import { closeEditPostForm, editPost } from "../actions";
 import Button from "./Button";
 
-class EditPostForm extends Component {
-  state = {
-    title: "",
-    body: ""
-  };
+const FormikForm = () => (
+  <div className="EditPostForm">
+    <h3>Edit Post</h3>
+    <Form>
+      <label>
+        Title:
+        <Field name="title" type="text" />
+      </label>
+      <label>
+        Body:
+        <Field component="textarea" name="body" />
+      </label>
+      <Button text="Update Post" />
+    </Form>
+  </div>
+);
 
-  componentDidMount() {
-    const { title, body } = this.props.post;
+const EditPostForm = withFormik({
+  mapPropsToValues({ post }) {
+    return {
+      title: post.title,
+      body: post.body
+    };
+  },
 
-    this.setState({
-      title: title,
-      body: body
+  handleSubmit(values, { props }) {
+    const { id } = props.post;
+    const { title, body } = values;
+
+    updatePost(id, { title: title, body: body }).then(post => {
+      props.dispatch(editPost(post));
+      props.dispatch(props.dispatch(closeEditPostForm(id)));
     });
   }
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    const { id } = this.props.post;
-    const { title, body } = this.state;
-
-    updatePost(id, {
-      title: title,
-      body: body
-    }).then(post => {
-      this.props.dispatch(editPost(post));
-      this.props.dispatch(this.props.dispatch(closeEditPostForm(id)));
-    });
-  };
-
-  handleChange = e => {
-    e.preventDefault();
-    const target = e.target;
-
-    this.setState({
-      [target.name]: target.value
-    });
-  };
-
-  render() {
-    const { title, body } = this.state;
-
-    return (
-      <div className="EditPostForm">
-        <h3>Edit Post</h3>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Title:
-            <input
-              name="title"
-              type="text"
-              value={title}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            Body:
-            <textarea name="body" value={body} onChange={this.handleChange} />
-          </label>
-          <Button text={"Update Post"} />
-        </form>
-      </div>
-    );
-  }
-}
+})(FormikForm);
 
 function mapStateToProps(state, ownProps) {
   const post = state.posts[ownProps.id];
